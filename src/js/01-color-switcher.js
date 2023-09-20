73991ec7d2c915a24328a0873880697f12f8cf47
Agata Wajda <agawaj98@gmail.com>
2023-09-20 22:57:02 +0200

    task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const body = document.querySelector("body") ;
+const startBtn = document.querySelector("button[data-start]") ;
+const stopBtn = document.querySelector("button[data-stop]") ;
+let intervalColors  = null ;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  } ;
+
+  stopBtn.setAttribute("disabled" , "") ;
+
+  startBtn.addEventListener ("click" , () => {
+    intervalColors = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor() ; 
+  } , 1000) ;
+  startBtn.setAttribute("disabled" , "") ;
+  stopBtn.removeAttribute("disabled") ;
+  }) ;
+
+  stopBtn.addEventListener ("click" , () => {
+    clearInterval(intervalColors) ;
+    startBtn.removeAttribute("disabled") ;
+    stopBtn.setAttribute("disabled" , "") ;
+  })
 
